a211c527a5fec698c922a315bd1b23f0b9513b6a
Mahmoud Khalafallah <mahmoudkhalafallah92@gmail.com>
2017-08-09 15:04:23 +0200

    merge fix duplicate sources with fix googleplus redirection

--- src/ios/GooglePlus.m
+++ src/ios/GooglePlus.m
@@ -1,17 +1,7 @@
+//#import <GoogleOpenSource/GoogleOpenSource.h>
 #import "AppDelegate.h"
 #import "objc/runtime.h"
 #import "GooglePlus.h"
-
-/** Switch to Sign-In SDK.
- @date July 19, 2015
- @author Eddy Verbruggen
- */
-
-/** 
-  Updates to be more aligned with updated Android version and with Google.
- @date March 15, 2015
- @author Sam Muggleworth (PointSource, LLC)
- */
 
 // need to swap out a method, so swizzling it here
 static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector);
@@ -19,14 +9,26 @@ static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelect
 @implementation AppDelegate (IdentityUrlHandling)
 
 + (void)load {
-    swizzleMethod([AppDelegate class],
+  swizzleMethod([AppDelegate class],
                 @selector(application:openURL:sourceApplication:annotation:),
                 @selector(identity_application:openURL:sourceApplication:annotation:));
-
-    swizzleMethod([AppDelegate class],
-                @selector(application:openURL:options:),
-                @selector(indentity_application_options:openURL:options:));
 }
+
+//- (BOOL)identity_application: (UIApplication *)application
+//                     openURL: (NSURL *)url
+//           sourceApplication: (NSString *)sourceApplication
+//                  annotation: (id)annotation {
+//
+//  GooglePlus* gp = (GooglePlus*)[[self.viewController pluginObjects] objectForKey:@"GooglePlus"];
+//
+//  if ([gp isSigningIn]) {
+//    gp.isSigningIn = NO;
+//    return [GPPURLHandler handleURL:url sourceApplication:sourceApplication annotation:annotation];
+//  } else {
+//    // call super
+//    return [self identity_application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+//  }
+//}
 
 /** Google Sign-In SDK
  @date July 19, 2015
@@ -35,7 +37,7 @@ static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelect
                      openURL: (NSURL *)url
            sourceApplication: (NSString *)sourceApplication
                   annotation: (id)annotation {
-    GooglePlus* gp = (GooglePlus*) [self.viewController pluginObjects][@"GooglePlus"];
+    GooglePlus* gp = (GooglePlus*)[[self.viewController pluginObjects] objectForKey:@"GooglePlus"];
 
     if ([gp isSigningIn]) {
         gp.isSigningIn = NO;
@@ -45,29 +47,12 @@ static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelect
         return [self identity_application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
     }
 }
-
-/**
-From https://github.com/EddyVerbruggen/cordova-plugin-googleplus/issues/227#issuecomment-227674026
-Fixes issue with G+ login window not closing correctly on ios 9
-*/
-- (BOOL)indentity_application_options: (UIApplication *)app
-            openURL: (NSURL *)url
-            options: (NSDictionary *)options
-{
-    GooglePlus* gp = (GooglePlus*) [self.viewController pluginObjects][@"GooglePlus"];
-
-    if ([gp isSigningIn]) {
-        gp.isSigningIn = NO;
-        return [[GIDSignIn sharedInstance] handleURL:url
-            sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-            annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
-    } else {
-        // Other
-        return [self application:app openURL:url
-            sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-            annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
-    }
+-(BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
+    return [[GIDSignIn sharedInstance] handleURL:url
+                               sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                      annotation:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
 }
+
 @end
 
 @implementation GooglePlus
@@ -76,28 +61,66 @@ Fixes issue with G+ login window not closing correctly on ios 9
 // see https://code.google.com/p/google-plus-platform/issues/detail?id=900
 // Update: should be fine since we use the GoogleSignIn framework instead of the GooglePlus framework
 - (void) isAvailable:(CDVInvokedUrlCommand*)command {
+//  BOOL appInstalled = [[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:@"gplus://"]];
   CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void) login:(CDVInvokedUrlCommand*)command {
+  self.isSigningIn = YES;
+//  [[self getGooglePlusSignInObject:command] authenticate];
   [[self getGIDSignInObject:command] signIn];
 }
+
+/** Switch to Sign-In SDK.
+ @date July 19, 2015
+ */
+//- (void) trySilentLogin:(CDVInvokedUrlCommand*)command {
+//  // trySilentAuthentication doesn't call delegate when it fails, so handle it here
+//  if (![[self getGooglePlusSignInObject:command] trySilentAuthentication]) {
+//    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no valid token"];
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//  }
+//}
 
 /** Get Google Sign-In object
  @date July 19, 2015
  */
 - (void) trySilentLogin:(CDVInvokedUrlCommand*)command {
+    self.isSigningIn = YES;
     [[self getGIDSignInObject:command] signInSilently];
 }
 
+/** Switch to Sign-In SDK
+ @date July 19, 2015
+ */
+//- (GPPSignIn*) getGooglePlusSignInObject:(CDVInvokedUrlCommand*)command {
+//  _callbackId = command.callbackId;
+//  NSDictionary* options = [command.arguments objectAtIndex:0];
+//  NSString* apiKey = [options objectForKey:@"iOSApiKey"];
+//  if (apiKey == nil) {
+//    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"iOSApiKey not set"];
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
+//    return nil;
+//  }
+//
+//  GPPSignIn *signIn = [GPPSignIn sharedInstance];
+//  signIn.shouldFetchGooglePlusUser = YES;
+//  signIn.shouldFetchGoogleUserEmail = YES;
+//  signIn.shouldFetchGoogleUserID = YES;
+//  signIn.clientID = apiKey;
+//  signIn.scopes = @[kGTLAuthScopePlusLogin];
+//  signIn.attemptSSO = YES; // tries to use other installed Google apps
+//  signIn.delegate = self;
+//  return signIn;
+//}
+
 /** Get Google Sign-In object
  @date July 19, 2015
- @date updated March 15, 2015 (@author PointSource,LLC)
  */
 - (GIDSignIn*) getGIDSignInObject:(CDVInvokedUrlCommand*)command {
     _callbackId = command.callbackId;
-    NSDictionary* options = command.arguments[0];
+    NSDictionary* options = [command.arguments objectAtIndex:0];
     NSString *reversedClientId = [self getreversedClientId];
 
     if (reversedClientId == nil) {
@@ -108,26 +131,17 @@ Fixes issue with G+ login window not closing correctly on ios 9
 
     NSString *clientId = [self reverseUrlScheme:reversedClientId];
 
-    NSString* scopesString = options[@"scopes"];
-    NSString* serverClientId = options[@"webClientId"];
-    NSString *loginHint = options[@"loginHint"];
-    BOOL offline = [options[@"offline"] boolValue];
-    NSString* hostedDomain = options[@"hostedDomain"];
-
+    NSString* scopesString = [options objectForKey:@"scopes"];
+    NSString* serverClientId = [options objectForKey:@"webApiKey"];
 
     GIDSignIn *signIn = [GIDSignIn sharedInstance];
     signIn.clientID = clientId;
 
-    [signIn setLoginHint:loginHint];
-
-    if (serverClientId != nil && offline) {
+    if (serverClientId != nil) {
       signIn.serverClientID = serverClientId;
     }
-    
-    if (hostedDomain != nil) {
-        signIn.hostedDomain = hostedDomain;
-    }
 
+    signIn.allowsSignInWithBrowser = NO; // Otherwise your app get rejected
     signIn.uiDelegate = self;
     signIn.delegate = self;
 
@@ -136,6 +150,8 @@ Fixes issue with G+ login window not closing correctly on ios 9
         NSArray* scopes = [scopesString componentsSeparatedByString:@" "];
         [signIn setScopes:scopes];
     }
+    [signIn setAllowsSignInWithBrowser:NO]; // disabling as this may be a rejection reason for Apple
+    [signIn setAllowsSignInWithWebView:YES]; // assuming this should be fine
     return signIn;
 }
 
@@ -151,11 +167,11 @@ Fixes issue with G+ login window not closing correctly on ios 9
 
   if (URLTypes != nil) {
     for (NSDictionary* dict in URLTypes) {
-      NSString *urlName = dict[@"CFBundleURLName"];
+      NSString *urlName = [dict objectForKey:@"CFBundleURLName"];
       if ([urlName isEqualToString:@"REVERSED_CLIENT_ID"]) {
-        NSArray* URLSchemes = dict[@"CFBundleURLSchemes"];
+        NSArray* URLSchemes = [dict objectForKey:@"CFBundleURLSchemes"];
         if (URLSchemes != nil) {
-          return URLSchemes[0];
+          return [URLSchemes objectAtIndex:0];
         }
       }
     }
@@ -179,6 +195,50 @@ Fixes issue with G+ login window not closing correctly on ios 9
   // for a rainy day.. see for a (limited) example https://github.com/vleango/GooglePlus-PhoneGap-iOS/blob/master/src/ios/GPlus.m
 }
 
+
+/** Switch to Sign-In SDK
+ @date July 19, 2015
+ */
+//- (void)finishedWithAuth:(GTMOAuth2Authentication *)auth
+//                   error:(NSError *)error {
+//  if (error) {
+//    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
+//  } else {
+//    NSString *email = [GPPSignIn sharedInstance].userEmail;
+//    NSString *token = [GPPSignIn sharedInstance].idToken;
+//    GTMOAuth2Authentication *auth = [[GPPSignIn sharedInstance] authentication];
+//    NSString *accessToken = auth.accessToken;
+//    NSString *userId = [GPPSignIn sharedInstance].userID;
+//    GTLPlusPerson *person = [GPPSignIn sharedInstance].googlePlusUser;
+//    NSDictionary *result;
+//
+//    if (person == nil) {
+//      result = @{
+//                 @"email" : email
+//                 };
+//    } else {
+//      result = @{
+//                 @"email"       : email,
+//                 @"idToken"     : token,
+//                 @"oauthToken"  : accessToken,
+//                 @"userId"      : userId,
+//                 @"displayName" : person.displayName ?: [NSNull null],
+//                 @"gender"      : person.gender ?: [NSNull null],
+//                 @"imageUrl"    : (person.image != nil && person.image.url != nil) ? person.image.url : [NSNull null],
+//                 @"givenName"   : (person.name != nil && person.name.givenName != nil) ? person.name.givenName : [NSNull null],
+//                 @"middleName"  : (person.name != nil && person.name.middleName != nil) ? person.name.middleName : [NSNull null],
+//                 @"familyName"  : (person.name != nil && person.name.familyName != nil) ? person.name.familyName : [NSNull null],
+//                 @"ageRangeMin" : person.ageRange && person.ageRange.min ? person.ageRange.min : [NSNull null],
+//                 @"ageRangeMax" : person.ageRange && person.ageRange.max ? person.ageRange.max : [NSNull null],
+//                 @"birthday"    : person.birthday ?: [NSNull null]
+//                 };
+//    }
+//    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
+//  }
+//}
+
 #pragma mark - GIDSignInDelegate
 /** Google Sign-In SDK
  @date July 19, 2015
@@ -189,25 +249,32 @@ Fixes issue with G+ login window not closing correctly on ios 9
         [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
     } else {
         NSString *email = user.profile.email;
-        NSString *idToken = user.authentication.idToken;
+        NSString *token = user.authentication.idToken;
         NSString *accessToken = user.authentication.accessToken;
         NSString *refreshToken = user.authentication.refreshToken;
         NSString *userId = user.userID;
         NSString *serverAuthCode = user.serverAuthCode != nil ? user.serverAuthCode : @"";
+//        GTLPlusPerson *person = [GPPSignIn sharedInstance].googlePlusUser;
         NSURL *imageUrl = [user.profile imageURLWithDimension:120]; // TODO pass in img size as param, and try to sync with Android
         NSDictionary *result = @{
-                       @"email"           : email,
-                       @"idToken"         : idToken,
-                       @"serverAuthCode"  : serverAuthCode,
-                       @"accessToken"     : accessToken,
-                       @"refreshToken"    : refreshToken,
-                       @"userId"          : userId,
-                       @"displayName"     : user.profile.name       ? : [NSNull null],
-                       @"givenName"       : user.profile.givenName  ? : [NSNull null],
-                       @"familyName"      : user.profile.familyName ? : [NSNull null],
-                       @"imageUrl"        : imageUrl ? imageUrl.absoluteString : [NSNull null],
+                       @"email"       : email,
+                       @"idToken"     : token,
+                       @"oauthToken"  : serverAuthCode,
+                       @"accessToken" : accessToken,
+                       @"refreshToken": refreshToken,
+                       @"userId"      : userId,
+                       @"displayName" : user.profile.name ? : [NSNull null],
+                       @"imageUrl"    : imageUrl ? imageUrl.absoluteString : [NSNull null],
+                       /*,
+                       @"gender"      : person.gender ?: [NSNull null],
+                       @"givenName"   : (person.name != nil && person.name.givenName != nil) ? person.name.givenName : [NSNull null],
+                       @"middleName"  : (person.name != nil && person.name.middleName != nil) ? person.name.middleName : [NSNull null],
+                       @"familyName"  : (person.name != nil && person.name.familyName != nil) ? person.name.familyName : [NSNull null],
+                       @"ageRangeMin" : person.ageRange && person.ageRange.min ? person.ageRange.min : [NSNull null],
+                       @"ageRangeMax" : person.ageRange && person.ageRange.max ? person.ageRange.max : [NSNull null],
+                       @"birthday"    : person.birthday ?: [NSNull null]*/
                        };
-
+//        }
         CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:_callbackId];
     }
@@ -217,7 +284,6 @@ Fixes issue with G+ login window not closing correctly on ios 9
  @date July 19, 2015
  */
 - (void)signIn:(GIDSignIn *)signIn presentViewController:(UIViewController *)viewController {
-    self.isSigningIn = YES;
     [self.viewController presentViewController:viewController animated:YES completion:nil];
 }
 
